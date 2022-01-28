2139d1cc657f5a157b4fe4877cb6d8e09e490980
unknown <lipesantossimao@gmail.com>
2022-01-27 23:08:23 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var button =document.querySelector('button').addEventListener('click', ()=>{
+    
+    let item = document.querySelector('input')
+    addItem(item.value)
+    item.value = ''
+
+})
+
+function addItem(item){
+
+    let list = document.querySelector('ul')
+    list.innerHTML += `<li>${item}</li>`
+
+}
